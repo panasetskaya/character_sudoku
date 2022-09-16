7a78b4d367b2c163f48b750d62053c42b96a079e
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-09-16 16:14:16 +0800

    add SudokuBoardView

--- app/src/main/java/com/panasetskaia/charactersudoku/presentation/MainViewModel.kt
+++ app/src/main/java/com/panasetskaia/charactersudoku/presentation/MainViewModel.kt
@@ -5,13 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.panasetskaia.charactersudoku.domain.Level
 import com.panasetskaia.charactersudoku.domain.SudokuGame
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
 class MainViewModel: ViewModel() {
 
     fun getGame(level: Level) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.Default) {
             val map = SudokuGame().fillGrid(level)
             Log.d("MY_TAG",map.toString())
         }
