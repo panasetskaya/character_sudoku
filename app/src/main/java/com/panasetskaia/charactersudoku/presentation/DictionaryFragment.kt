f9ea9511cf0106fdc985c9ab00dab9462e358338
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-09-28 14:38:10 +0800

    add GameFragment

--- app/src/main/java/com/panasetskaia/charactersudoku/presentation/DictionaryFragment.kt
+++ app/src/main/java/com/panasetskaia/charactersudoku/presentation/DictionaryFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import com.panasetskaia.charactersudoku.R
+import com.panasetskaia.charactersudoku.databinding.FragmentDictionaryBinding
 
 class DictionaryFragment : Fragment() {
+
+    private var _binding: FragmentDictionaryBinding? = null
+    private val binding: FragmentDictionaryBinding
+        get() = _binding ?: throw RuntimeException("FragmentDictionaryBinding is null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,22 +20,13 @@ class DictionaryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dictionary, container, false)
+    ): View {
+        _binding = FragmentDictionaryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-
-
-    companion object {
-
-        @JvmStatic
-        fun newInstance() = DictionaryFragment()
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -38,5 +34,13 @@ class DictionaryFragment : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
+    companion object {
+        @JvmStatic
+        fun newInstance() = DictionaryFragment()
+    }
 }
--- app/src/main/java/com/panasetskaia/charactersudoku/presentation/MainActivity.kt
+++ app/src/main/java/com/panasetskaia/charactersudoku/presentation/MainActivity.kt
@@ -1,88 +1,24 @@
 package com.panasetskaia.charactersudoku.presentation
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.os.Bundle
-import android.view.View
-import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.panasetskaia.charactersudoku.databinding.ActivityMainBinding
-import com.panasetskaia.charactersudoku.domain.entities.Cell
-import com.panasetskaia.charactersudoku.presentation.customViews.SudokuBoardView
 
-class MainActivity : AppCompatActivity(), SudokuBoardView.OnTouchListener {
+class MainActivity : AppCompatActivity() {
 
-    private val viewModel by lazy {
+    val viewModel by lazy {
         ViewModelProvider(this)[GameViewModel::class.java]
     }
-    private lateinit var binding: ActivityMainBinding
 
-    private val linearInterpolator = LinearInterpolator()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.sudokuBoard.registerListener(this)
-        viewModel.selectedCellLiveData.observe(this) {
-            updateSelectedCellUI(it)
-        }
-        viewModel.boardLiveData.observe(this) {
-            updateCells(it.cells)
-        }
-        val buttons = listOf(
-            binding.oneButton,
-            binding.twoButton,
-            binding.threeButton,
-            binding.fourButton,
-            binding.fiveButton,
-            binding.sixButton,
-            binding.sevenButton,
-            binding.eightButton,
-            binding.nineButton
-        )
-
-
-        for (button in buttons) {
-            button.text = viewModel.nineCharacters[buttons.indexOf(button)]
-        }
-        buttons.forEachIndexed { index, button ->
-            button.setOnClickListener {
-                viewModel.handleInput(index)
-                AnimatorSet().apply {
-                    play(shakeAnimator(it, "rotation"))
-                    start()
-                }
-            }
-        }
     }
 
-    override fun onCellTouched(row: Int, col: Int) {
-        viewModel.updateSelection(row, col)
-    }
+    //TODO: добавить фрагмент-контейнер
 
-    override fun onCellLongTouched(row: Int, col: Int) {
-        viewModel.updateSelection(row, col)
-        viewModel.markSelectedAsDoubtful()
-
-    }
-
-    private fun updateSelectedCellUI(cell: Pair<Int, Int>?) = cell?.let {
-        binding.sudokuBoard.updateSelectedCellUI(cell.first, cell.second)
-    }
-
-    private fun updateCells(cells: List<Cell>?) = cells?.let {
-        binding.sudokuBoard.updateCells(cells)
-    }
-
-
-    private fun shakeAnimator(shake: View, propertyName: String) =
-        ObjectAnimator.ofFloat(shake, propertyName, -5f, 0f).apply {
-            repeatMode = ValueAnimator.RESTART
-            repeatCount = 2
-            duration = 40
-            interpolator = linearInterpolator
-        }
 }
