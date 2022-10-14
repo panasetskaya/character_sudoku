bb960c3c690d873383ed17a139c00af18b4b1e9b
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-10-14 12:33:23 +0800

    small fix

--- app/src/main/java/com/panasetskaia/charactersudoku/application/SudokuApplication.kt
+++ app/src/main/java/com/panasetskaia/charactersudoku/application/SudokuApplication.kt
@@ -5,16 +5,6 @@ import com.panasetskaia.charactersudoku.di.DaggerAppComponent
 
 class SudokuApplication: Application() {
 
-    companion object {
-        lateinit var instance: SudokuApplication
-            private set
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-    }
-
     val component by lazy {
         DaggerAppComponent.factory().create(this)
     }
