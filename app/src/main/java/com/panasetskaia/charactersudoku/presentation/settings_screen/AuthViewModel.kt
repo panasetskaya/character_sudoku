b6a120533c3f2bbccb913da5bfb6b1965c8480cb
Maria Panasetskaia <mariapolikutina@gmail.com>
2023-08-18 13:53:40 +0200

    add sign-in with email or google flow

--- app/src/main/java/com/panasetskaia/charactersudoku/presentation/settings_screen/AuthViewModel.kt
+++ app/src/main/java/com/panasetskaia/charactersudoku/presentation/settings_screen/AuthViewModel.kt
@@ -1,7 +1,11 @@
 package com.panasetskaia.charactersudoku.presentation.settings_screen
 
 import android.app.Activity
+import android.content.Intent
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.panasetskaia.charactersudoku.R
 import com.panasetskaia.charactersudoku.presentation.base.BaseViewModel
 import com.panasetskaia.charactersudoku.utils.Event
@@ -10,7 +14,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
-class AuthViewModel @Inject constructor(): BaseViewModel()  {
+class AuthViewModel @Inject constructor() : BaseViewModel() {
 
     private val _toastFlow = MutableStateFlow<Event<Int>?>(null)
     val toastFlow: StateFlow<Event<Int>?>
@@ -24,21 +28,58 @@ class AuthViewModel @Inject constructor(): BaseViewModel()  {
     }
 
     fun goToSignUp() {
-    //todo
-    // navigate(SignInFragmentDirections.actionSignInFragmentToSignUpFragment())
+        //todo
+        // navigate(SignInFragmentDirections.actionSignInFragmentToSignUpFragment())
     }
 
     fun signInWithEmail(auth: FirebaseAuth, email: String, password: String, activity: Activity) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(activity) { task ->
-                if (task.isSuccessful) {
-                    myLog("signInWithEmail:success")
-                    _isUserSignedInFlow.value = true
-                } else {
-                    myLog("signInWithEmail:failure: ${task.exception}")
-                    _toastFlow.value = Event(R.string.auth_failed)
-                    _isUserSignedInFlow.value = false
+        if (email!="" && password!="") {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity) { task ->
+                    if (task.isSuccessful) {
+                        myLog("signInWithEmail:success")
+                        _isUserSignedInFlow.value = true
+                    } else {
+                        myLog("signInWithEmail:failure: ${task.exception}")
+                        _toastFlow.value = Event(R.string.auth_failed)
+                        _isUserSignedInFlow.value = false
+                    }
+                }
+        }
+    }
+
+    fun signInWithGoogle(
+        auth: FirebaseAuth,
+        oneTapClient: SignInClient,
+        data: Intent?,
+        activity: Activity
+    ) {
+        try {
+            val credential = oneTapClient.getSignInCredentialFromIntent(data)
+            val idToken = credential.googleIdToken
+            when {
+                idToken != null -> {
+                    myLog("signInWithGoogle: Got ID token.")
+                    val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                    auth.signInWithCredential(firebaseCredential)
+                        .addOnCompleteListener(activity) { task ->
+                            if (task.isSuccessful) {
+                                myLog("signInWithGoogle:success")
+                                _toastFlow.value = Event(R.string.welcome)
+                                _isUserSignedInFlow.value = true
+                            } else {
+                                myLog("signInWithCredential:failure: ${task.exception}")
+                                _toastFlow.value = Event(R.string.auth_failed)
+                                _isUserSignedInFlow.value = false
+                            }
+                        }
+                }
+                else -> {
+                    myLog("signInWithGoogle:failure: no ID token!")
                 }
             }
+        } catch (e: Exception) {
+            myLog("signInWithGoogle:failure: $e ${e.message}")
+        }
     }
 }
--- app/src/main/java/com/panasetskaia/charactersudoku/presentation/settings_screen/SignInFragment.kt
+++ app/src/main/java/com/panasetskaia/charactersudoku/presentation/settings_screen/SignInFragment.kt
@@ -1,18 +1,25 @@
 package com.panasetskaia.charactersudoku.presentation.settings_screen
 
 import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.panasetskaia.charactersudoku.R
 import com.panasetskaia.charactersudoku.databinding.FragmentSignInBinding
 import com.panasetskaia.charactersudoku.presentation.base.BaseFragment
 import com.panasetskaia.charactersudoku.presentation.viewmodels.ViewModelFactory
 import com.panasetskaia.charactersudoku.utils.getAppComponent
+import com.panasetskaia.charactersudoku.utils.myLog
 import com.panasetskaia.charactersudoku.utils.toast
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -28,6 +35,10 @@ class SignInFragment : BaseFragment<FragmentSignInBinding, AuthViewModel>(
     override val viewModel by viewModels<AuthViewModel> { viewModelFactory }
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+
+    private val REQ_ONE_TAP = 2
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -35,17 +46,59 @@ class SignInFragment : BaseFragment<FragmentSignInBinding, AuthViewModel>(
     }
 
     override fun onReady(savedInstanceState: Bundle?) {
-        auth = Firebase.auth
+        setupAuth()
         setListeners()
         collectFlows()
     }
 
+    private fun setupAuth() {
+        auth = Firebase.auth
+        oneTapClient = Identity.getSignInClient(requireActivity())
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.web_client_id))
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+    }
+
     private fun setListeners() {
         binding.buttonSignIn.setOnClickListener {
-            val email = binding.etEmail.text.toString().trim()
-            val password = binding.etPassword.text.toString().trim()
-            viewModel.signInWithEmail(auth, email, password, requireActivity())
+            launchEmailSignIn()
         }
+        binding.googleSignInButton.setOnClickListener {
+            launchGoogleSignIn()
+        }
+        binding.tvSignUp.setOnClickListener {
+            viewModel.goToSignUp()
+        }
+    }
+
+    private fun launchEmailSignIn() {
+        val email = binding.etEmail.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+        viewModel.signInWithEmail(auth, email, password, requireActivity())
+    }
+
+    private fun launchGoogleSignIn() {
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(requireActivity()) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null)
+                } catch (e: IntentSender.SendIntentException) {
+                    myLog( "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(requireActivity()) { e ->
+                myLog(e.localizedMessage)
+            }
+
     }
 
     private fun collectFlows() {
@@ -65,6 +118,15 @@ class SignInFragment : BaseFragment<FragmentSignInBinding, AuthViewModel>(
                         }
                     }
                 }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                viewModel.signInWithGoogle(auth, oneTapClient, data, requireActivity())
             }
         }
     }
